2cc65877c7182dc55834d5c84fcf7096d951b3ec
n286f7kh2k-debug <n286f7kh2k@privaterelay.appleid.com>
2026-08-16 17:34:03 -0400

    Add fixed ThemeArt staging file

---
+++ native-race-day-live/src/MXBRaceDayLive.Profile/ThemeArt.fixed.cs
@@ -0,0 +1,8 @@
+using System.IO;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace MXBRaceDayLive.Profile;
+
+internal static class ThemeArtFixedPlaceholder { }
